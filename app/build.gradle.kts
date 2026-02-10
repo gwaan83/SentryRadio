c0908140aa2b315b11ee81d9f41838a82d21faf3
fzer0x <57650139+fzer0x@users.noreply.github.com>
2026-02-10 19:12:35 +0100

    Improve Security features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.fzer0x.imsicatcherdetector2"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
