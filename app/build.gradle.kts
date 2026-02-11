8b2d08e42f2c413b744522bd267f24a871922004
fzer0x <57650139+fzer0x@users.noreply.github.com>
2026-02-11 10:53:05 +0100

    increase versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.fzer0x.imsicatcherdetector2"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.2"
+        versionCode = 8
+        versionName = "0.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
